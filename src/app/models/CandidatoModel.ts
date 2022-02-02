b6a2979a587ae5fe7f71aba57d02dfe8f94f0183
Hugo Esteves <hugo.esteves@unesp.br>
2022-02-01 23:12:52 -0300

    Criação de campos para os candidatos, relação com usuário e empresa

--- src/app/models/CandidatoModel.ts
+++ src/app/models/CandidatoModel.ts
@@ -1,41 +1,51 @@
 import { Column, CreateDateColumn, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { SituacaoCandidato } from "../enums/SituacaoCandidatoEnum";
 import { BaseModel } from "./baseModel";
+import { Empresa } from "./EmpresaModel";
 import { Endereco } from "./EnderecoModel";
 import { Formacao } from "./FormacaoModel";
 import { Habilidade } from "./HabilidadeModel";
+import { Usuario } from "./UsuarioModel";
 
 @Entity()
 export class Candidato extends BaseModel {
     @Column()
     code: number;
-    
+
     @Column()
     name: string;
-    
-    @Column({type: "date"})
-    dataNascimento: Date;
-    
+
+    @Column({ type: "date" })
+    data_nascimento: Date;
+
     @Column()
     email: string;
-    
+
     @Column()
     telefone: string;
-    
+
+    @Column({
+        type: "enum",
+        enum: SituacaoCandidato,
+    })
+    situacao_candidato: SituacaoCandidato;
+
     @ManyToOne(() => Endereco, endereco => endereco.candidato)
     endereco: Endereco;
 
     @ManyToMany(() => Habilidade)
     @JoinTable()
     habilidades: Habilidade[];
-    
-    @ManyToOne(() => Formacao, formacao => formacao.candidato, {cascade: true})
+
+    @ManyToOne(() => Formacao, formacao => formacao.candidato, { cascade: true })
     formacao: Formacao;
 
-    @Column({type: 'date'})
-    @CreateDateColumn()
-    create_date: Date;
-    
-    @Column({type: 'date'})
-    @UpdateDateColumn()
-    update_date: Date;
+    @ManyToOne(() => Empresa, empresa => empresa.candidatos)
+    empresa: Empresa;
+
+    @ManyToOne(() => Usuario, usuario => usuario.candidatos)
+    created_user: Usuario;
+
+    @ManyToOne(() => Usuario, usuario => usuario.candidatos)
+    responsible_user: Usuario;
 }
