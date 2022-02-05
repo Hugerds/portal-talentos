4a3c85c862dd9da2b6a19c09778e132e52fa9202
Hugo Esteves <hugo.esteves@unesp.br>
2022-02-05 19:53:01 -0300

    Middleware para autenticação e jwt

---
+++ src/middlewares/AuthMiddlaware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken';
+import { UnauthorizedException } from "../Errors/unauthorizedException";
+
+interface TokenPayLoad {
+    id: string,
+    iat: number,
+    exp: number
+}
+
+//Middleware para autenticação, é chamado em todas as rotas autenticadas
+export default function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        throw new UnauthorizedException("Não Autorizado");
+    }
+
+    const token = authorization.replace('Bearer', '').trim();
+
+    try {
+        const data = jwt.verify(token, process.env.TOKEN_JWT);
+
+        const { id } = data as TokenPayLoad;
+
+        req.userId = id;
+        return next();
+    } catch {
+        throw new UnauthorizedException("Não Autorizado");
+    }
+}
